a6eb33682c5ad724375fa88713d3cbb0a6dd5be9
daya <daya.sekar@yahoo.com>
2020-06-29 12:39:37 +0800

    Initial commit

---
+++ src/main/java/org/hack20/AppGatewayApplication.java
@@ -0,0 +1,14 @@
+package org.hack20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class AppGatewayApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(AppGatewayApplication.class);
+    }
+}
